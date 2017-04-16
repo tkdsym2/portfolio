757b91484db91b97e69934aad785e7028613b8d2
fmsuvM <k222.tkd.fms@gmail.com>
2017-04-16 18:37:19 +0900

    change route file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Match,
+    Switch,
+    IndexRoute
 } from 'react-router-dom';
 
-import Home from './javascripts/pages/Home.jsx';
+import Works from './javascripts/pages/Works.jsx';
+import About from './javascripts/pages/About.jsx';
+import Header from './javascripts/pages/Header.jsx';
+import Footer from './javascripts/pages/Footer.jsx';
 
 class App extends Component {
-    
-    render(){
+    render() {
         return (
-            <div>
-                <Home />
-            </div>
+            <Router>
+                <div className="grid-container">
+
+                    <Header />
+
+                    <Route exact path="/" component={Works}/>
+                    <Route path="/works" component={Works} />
+                    <Route path="/about" component={About}/>
+
+                    <Footer/>
+                </div>
+            </Router>
         )
     }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+export default App;
