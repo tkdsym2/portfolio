adb4d14deafc2acafe6ae2f2e8ee4327e4277b92
fmsuvM <k222.tkd.fms@gmail.com>
2017-04-16 18:38:49 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,50 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = [{
-        entry: path.join(__dirname, 'src/App.jsx'),
-        output: {
-            path: path.join(__dirname, 'dist'),
-            filename: 'bundle.js'
-        },
-        module: {
-            loaders: [{
-                test: /.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }]
-        },
-        resolve: {
-            extensions: ['.js', ',jsx']
-        },
+    entry: path.join(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
-    {
-        entry: path.join(__dirname, 'src/stylesheets/index.sass'),
-        output: {
-            path: path.join(__dirname, 'dist'),
-            filename: 'index.css'
-        },
-        module: {
-            loaders: [{
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
-                })
-            }]
-        },
-        plugins: [
-            new ExtractTextPlugin('index.css')
-        ]
+    module: {
+        loaders: [{
+            test: /\.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        // hot: true,
+        contentBase: '.',
+        port: 3000,
+        // host: '0.0.0.0',
+        inline: true
     }
-]
+}, {
+    context: path.join(__dirname, 'src/stylesheets/'),
+    entry: {
+        app: './index.scss'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.css'
+    },
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader?-url&sourceMap&minimize!sass-loader?outputStyle=expanded&sourceMap=tr" +
+                    "ue&sourceMapContents=true"
+            })
+        }]
+    },
+    plugins: [new ExtractTextPlugin('index.css')],
+    devtool: 'source-map'
+}]
