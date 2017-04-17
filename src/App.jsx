221047a2a5bf1dcec734cc9e1dd6f49b377be530
fmsuvM <k222.tkd.fms@gmail.com>
2017-04-17 11:33:35 +0900

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import Radium from 'radium';
 import {
     BrowserRouter as Router,
     Route,
@@ -19,8 +19,25 @@ class App extends Component {
         return (
             <Router>
                 <div className="grid-container">
-
-                    <Header />
+                    <header className="globalHeader header_wrap" style={[styles.base]}>
+                    <nav className="navi">
+                        <ul className="navicontents">
+                            <li>
+                                <h1>This is Header</h1>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Works
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/about">
+                                    About
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
 
                     <Route exact path="/" component={Works}/>
                     <Route path="/works" component={Works} />
@@ -33,4 +50,43 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
+
+const styles = {
+    base: {
+        backgroundColor: '#fff',
+        height: '200px',
+        outline: 'none',
+        paddingTop: '25px',
+        paddingLeft: '20px',
+        boxShadow: '0 3px 0 rgba(0,0,0,0.2)'
+    },
+    title: {
+        color: '#333',
+        fontSize: 40,
+        fontFamily: 'Capella Bold',
+        marginLeft: '10px'
+    },
+    works: {
+        fontSize: 20,
+        fontFamily: 'Capella Bold',
+        marginLeft: '50px',
+        ':hover': {
+            backgroundColor: '#FF0000'
+        }
+    },
+    about: {
+        fontSize: 20,
+        fontFamily: 'Capella Bold',
+        marginLeft: '20px'
+    },
+    mainnav: {
+        marginTop: '30px',
+        textDecoration: 'none',
+        color: '#333',
+        letterSpacing: '0',
+        fontWeight: '400',
+        paddingTop: '150px',
+        paddingBottom: '54px'
+    }
+};
