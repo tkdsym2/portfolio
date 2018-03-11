b875282dbff4656677be63204929faa4947736a4
fmsuvM <k222.tkd.fms@gmail.com>
2018-03-11 14:44:23 +0900

    Feat: add dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    /*
+    ** Headers of the page
+    */
     head: {
         title: 'fmsuvM portfolio',
         meta: [
@@ -7,12 +10,25 @@ module.exports = {
                 name: 'viewport',
                 content: 'width=device-width, initial-scale=1'
             },
-            { hid: 'description', name: 'description', content: 'portfolio' }
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Nuxt.js project'
+            }
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     },
+    /*
+    ** Customize the progress bar color
+    */
     loading: { color: '#3B8070' },
+    /*
+    ** Build configuration
+    */
     build: {
+        /*
+      ** Run ESLint on save
+      */
         extend(config, { isDev, isClient }) {
             if(isDev && isClient) {
                 config.module.rules.push({
