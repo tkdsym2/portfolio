6377edc4b8a75f69546e470656a146ab2f839043
fmsuvM <k222.tkd.fms@gmail.com>
2018-03-11 14:22:15 +0900

    fix code by linter

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,27 @@
 module.exports = {
-  head: {
-    title: "fmsuvM portfolio",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "portfolio" }
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-  },
-  loading: { color: "#3B8070" },
-  build: {
-    extend(config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/
-        });
-      }
+    head: {
+        title: 'fmsuvM portfolio',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            { hid: 'description', name: 'description', content: 'portfolio' }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+    loading: { color: '#3B8070' },
+    build: {
+        extend(config, { isDev, isClient }) {
+            if(isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                });
+            }
+        }
     }
-  }
 };
