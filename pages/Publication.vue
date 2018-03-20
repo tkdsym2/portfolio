e35a7f05d7bf713a1db5cc0448b4b2913c82a8d6
fmsuvM <k222.tkd.fms@gmail.com>
2018-03-20 12:46:19 +0900

    Feat: update publications page style

--- pages/Publication.vue
+++ pages/Publication.vue
@@ -1,14 +1,50 @@
+<style src="~/style/pages/publication.scss" lang="scss" scoped></style>
+
 <template lang="pug">
-  div.content
-    h1.title Publication Page
-    p
-        nuxt-link(to="/") Top Page
+  div.research-page
+    h3 Publications
+    div.publications-container
+        div.domestic-conf
+            h3.title Domestic Conferences
+            div.contents
+                p 1. 髙田一真, 金子翔麻, 鈴木涼太, 神山洋一, 相澤裕貴, 大内泰良, 西條瞳, 高瀬理奈, 本田健人, 渡邊恵太.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;Flamework Framework: レシピデータに基づき 火力を制御するフレームワークの試作.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;第22回一般社団法人情報処理学会シンポジウム(インタラクション2018), 2018.
+                    a.publication-link(href="http://www.interaction-ipsj.org/proceedings/2018/data/pdf/1A13.pdf" target='_blank') &nbsp;&nbsp;&nbsp;PDF&nbsp;
+                    a.publication-link(href="http://www.interaction-ipsj.org/2018/program/" target='_blank') Web
+            div.contents
+                p 2. 大内泰良, 尾高陽太, 鈴木涼太, 神山洋一, 住朋享, 西條瞳, 吉村佳純, 金子翔麻, 片倉翔平, 高田一真, 高瀬理奈, 渡邊恵太.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;Ceres:朝の生活で利用するフードプリンタの提案と試作.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;第22回一般社団法人情報処理学会シンポジウム(インタラクション2018), 2018.
+                    a.publication-link(href="http://www.interaction-ipsj.org/proceedings/2018/data/pdf/1B60.pdf" target='_blank') &nbsp;&nbsp;&nbsp;PDF&nbsp;
+                    a.publication-link(href="http://www.interaction-ipsj.org/2018/program/" target='_blank') Web
+                        div.contents
+                p 3. 髙田一真, 渡邊恵太.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;被写通知：アドホックネットワークを利用した被写検知による写真共有.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;第24回インタラクティブシステムとソフトウェアに関するワークショップ(WISS2016), 2016.
+                    a.publication-link(href="http://www.wiss.org/WISS2016Proceedings/demo/3-A18.pdf" target='_blank') &nbsp;&nbsp;&nbsp;PDF&nbsp;
+                    a.publication-link(href="https://www.wiss.org/WISS2016/Demo.html" target='_blank') Web
+            div.contents
+                p 4. 髙田一真, 渡邊恵太.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;Sharetter: Bluetooth電波と顔認識を利用した被写検知に基づく写真共有.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;第169回ヒューマンコンピュータインタラクション研究発表会(HCI169), 2016.
+                    a.publication-link(href="http://www.ipsj.or.jp/kenkyukai/event/hci169.html" target='_blank') &nbsp;&nbsp;&nbsp;Web
+            div.contents
+                p 5. 髙田一真, 渡邊恵太.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;Sharetter : Bluetooth電波を利用した被写検知の検討と試作.
+                    br
+                    | &nbsp;&nbsp;&nbsp;&nbsp;第20回一般社団法人情報処理学会シンポジウム(インタラクション2016), 2016.
+                    a.publication-link(href="http://www.interaction-ipsj.org/proceedings/2016/data/pdf/2C47.pdf" target='_blank') &nbsp;&nbsp;&nbsp;PDF&nbsp;
+                    a.publication-link(href="http://www.interaction-ipsj.org/2016/program/interactive" target='_blank') Web
 </template>
 
-<script>
-export default {
-    head: {
-        title: 'Home Page'
-    }
-};
-</script>
+<script></script>
