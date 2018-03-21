ac99ea4b781baadd0495b98c6449f2b66515c495
fmsuvM <k222.tkd.fms@gmail.com>
2018-03-21 20:02:43 +0900

    Feat: add Card Component

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<style src="~/style/components/card.scss" lang="scss" scoped></style>
+
+<template lang="pug">
+    div.card-component
+        h3 {{ project.title }}
+        p {{ project.summary }}
+        nuxt-link(:to="{ path: '/Works/' + project.path}") read more
+</template>
+
+<script>
+import Debug from 'debug';
+
+const debug = Debug('components:card');
+
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
