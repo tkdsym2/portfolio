a4359a72d50c15fab59217aca11564bf92e966c7
fmsuvM <k222.tkd.fms@gmail.com>
2018-03-27 17:56:16 +0900

    Feat: refactor files

--- pages/Works/_project.vue
+++ pages/Works/_project.vue
@@ -1,45 +1,42 @@
-<style src="~/style/components/card.scss" lang="scss" scoped></style>
-
 <template lang="pug">
-    div.card-component
-        //- p {{ summaryData.data.title }}
-        <div v-html="summaryData.hd"></div>
+    div.project-page(v-html="summaryData.data.bodyHtml" v-bind:style="style")
 </template>
 
 <script>
 import Debug from 'debug';
 import { sourceFileArray } from '~/contents/summary.json';
-import h from 'hyperscript';
+import _style from '~/style/pages/_project.scss';
 
 const debug = Debug('works:project');
 
 export default {
     validate({ params }) {
-        debug(params);
         return sourceFileArray.includes(
             `contents/projects/${params.project}.md`
         );
     },
-    async asyncData({ params }) {
-        const _htmlData = h(
-            'div#page',
-            h('div#header', h('p', `${params.project}`))
-        );
-        const htmlData = _htmlData.outerHTML;
+    data() {
+        const params = {};
         const _data = Object.assign(
             {},
-            require(`~/contents/dist/${params.project}.json`),
+            require(`~/contents/dist/projects/${
+                this.$route.params.project
+            }.json`),
             {
                 params
             }
         );
         const summaryData = {
-            data: _data,
-            hd: htmlData
+            data: _data
         };
         return {
             summaryData
         };
+    },
+    computed: {
+        style() {
+            return _style;
+        }
     }
 };
 </script>
