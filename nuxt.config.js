4ee736679639b7626b3e6953d2835df72b1639c7
fmsuvM <k222.tkd.fms@gmail.com>
2018-04-03 13:50:42 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,63 @@
 module.exports = {
     /*
-    ** Headers of the page
-    */
+     ** Headers of the page
+     */
     head: {
         title: 'fmsuvM portfolio',
-        meta: [
-            {
-                charset: 'utf-8'
-            }, {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            }, {
-                hid: 'description',
-                name: 'description',
-                content: 'Kazuma Takada\'s Portfolio'
-            }
-        ],
-        link: [
-            {
-                rel: 'icon',
-                type: 'image/x-icon',
-                href: 'favicon.ico'
-            }
-        ]
+        meta: [{
+            charset: 'utf-8'
+        }, {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1'
+        }, {
+            hid: 'description',
+            name: 'description',
+            content: 'Kazuma Takada\'s Portfolio'
+        }],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: 'favicon.ico'
+        }]
     },
     css: ['~/style/global.css'],
     /*
-    ** Customize the progress bar color
-    */
+     ** Customize the progress bar color
+     */
     loading: {
         color: '#3B8070'
     },
+    router: {
+        base: '/fmsuvM.github.io/'
+    },
     /*
-    ** Build configuration
-    */
+     ** Build configuration
+     */
     build: {
         vendor: ['debug'],
         /*
-      ** Run ESLint on save
-      */
-        extend(config, {isDev, isClient, isServer}) {
+         ** Run ESLint on save
+         */
+        extend(config, {
+            isDev,
+            isClient,
+            isServer
+        }) {
             if (isDev && isClient) {
                 config
                     .module
                     .rules
-                    .push({enforce: 'pre', test: /\.(js|vue)$/, loader: 'eslint-loader', exclude: /(node_modules)/});
+                    .push({
+                        enforce: 'pre',
+                        test: /\.(js|vue)$/,
+                        loader: 'eslint-loader',
+                        exclude: /(node_modules)/
+                    });
             }
             if (isServer) {
-                config.externals = [require('webpack-node-externals')({whitelist: [/^vue-slick/]})]
+                config.externals = [require('webpack-node-externals')({
+                    whitelist: [/^vue-slick/]
+                })]
             }
         }
     }
