e304bb6e7aa46769d0e2d649e824de6b84ddb8e4
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-21 14:41:26 +0900

    Feat: add page components

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Navigator from "../components/Navigator";
+import { Test } from "../style/_";
+
+export default () => (
+  <Navigator title="portfolio">
+    <Test>About page</Test>
+  </Navigator>
+);
