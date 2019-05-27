971c6c20777f3f3965353da532d968c33f096933
tkdsym2 <k222.tkd.fms@gmail.com>
2019-05-27 15:30:01 +0900

    fix by eslinter

--- components/Navigator.tsx
+++ components/Navigator.tsx
@@ -1,35 +1,33 @@
-import * as React from 'react'
-import Link from "next/link";
-import Head from "next/head";
+import *as React from 'react'
+import Link from 'next/link';
+import Head from 'next/head';
 
-type Props = {
-  title?: string
+interface Props {
+    title?: string;
 }
 
-const Navigator: React.FunctionComponent<Props> = ({ children, title = 'default'}) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/contact">
-          <a>Contact</a>
-        </Link>
-      </nav>
-    </header>
-
-    {children}
-
-    <footer>{"I`m here to stay"}</footer>
+const Navigator: React.FunctionComponent<Props> = ({ children, title = 'default' }) => (
+    <div>
+        <Head>
+            <title>{title}</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <header>
+            <nav>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+                <Link href="/contact">
+                    <a>Contact</a>
+                </Link>
+            </nav>
+        </header>
+        {children}
+        <footer>{'I`m here to stay'}</footer>
     </div>
 )
 
