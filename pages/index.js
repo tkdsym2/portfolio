0e6a47cfcd2aa3fd3dc3758a08f3b362c892f4ed
tkdsym2 <k222.tkd.fms@gmail.com>
2019-05-27 13:42:42 +0900

    Fix: convert to tsx file

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import Navigator from "../components/Navigator";
-import { Test } from "../style/_";
-
-export default () => (
-  <Navigator title="portfolio">
-    <Test>hello hello</Test>
-  </Navigator>
-);
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+// import Link from 'next/link';
+import Navigator from '../components/Navigator';
+import { Test } from '../style/_';
+
+export default() => (
+    <Navigator title="portfolio">
+        <Test>hello hello</Test>
+    </Navigator>
+);
