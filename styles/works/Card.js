7cf2087f5d132edd6d73b55d823b1701f8d25347
tkdsym2 <k222.tkd.fms@gmail.com>
2020-01-19 20:49:56 +0900

    Feat: update card style

--- styles/works/Card.js
+++ styles/works/Card.js
@@ -1,22 +1,27 @@
 import styled from 'styled-components';
 import Link from 'next/link';
+import media from 'styled-media-query';
 
-import { CardSize, TopicSize, SentenceSize, Black, Gray } from '../theme'
+import { CardSize, TopicSize, SentenceSize, Black, Gray, MobileCardSize, huge, hhuge } from '../theme'
 
 const StyledCardFrame = styled.div`
-  width: ${CardSize};
+  /* width: ${CardSize}; */
+  width: 100%;
+  border: 1px solid #ddd;
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
   margin: 0;
-  margin: 0.5rem 0 0.5rem 0;
+  min-width: 0;
+  overflow: auto;
   &:hover {
     cursor: pointer;
     opacity: 0.6;
   }
 `
 
-export const CardFrame = ({ href, children, ...props }) => (
-  <Link href={href} passHref>
+export const CardFrame = ({ dir, url, children, ...props }) => (
+  <Link href={`/${dir}/[${url}]`} as={`${dir}/${url}`} passHref>
     <StyledCardFrame>
       {children}
     </StyledCardFrame>
@@ -24,7 +29,11 @@ export const CardFrame = ({ href, children, ...props }) => (
 )
 
 export const CardImage = styled.img`
-  width: ${CardSize};
+  width: 100%;
+  border-radius: 10px 10px 0 0;
+  /* ${media.lessThan(`${huge}`)`
+    width: ${MobileCardSize};
+  `} */
 `
 
 export const CardTitle = styled.p`
