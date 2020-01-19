b5e199c081453e6fdef739df1b58328f812bf329
tkdsym2 <k222.tkd.fms@gmail.com>
2020-01-19 20:50:11 +0900

    Feat: add variables

--- styles/theme.js
+++ styles/theme.js
@@ -1,3 +1,5 @@
+export const FrameSize = '70%';
+
 export const Black = '#000000';
 export const Gray = '#666666';
 export const Blue = '#0000EE';
@@ -9,7 +11,10 @@ export const TopicSize = '1.3rem';
 export const SentenceSize = '1.1rem';
 
 export const CardSize = '22rem';
+export const MobileCardSize = '28rem';
 
+export const huge = '1524px';
+export const hhuge = '1384px';
 export const large = '1450px';
 export const medium = '768px';
 export const small = '450px';
