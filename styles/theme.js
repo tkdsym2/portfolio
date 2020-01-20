ac1f66e02569f891b956005a36ccb8a429b1c3d7
tkdsym2 <k222.tkd.fms@gmail.com>
2020-01-20 11:12:36 +0900

    Feat: update color&letter size

--- styles/theme.js
+++ styles/theme.js
@@ -1,14 +1,15 @@
 export const FrameSize = '70%';
 
-export const Black = '#000000';
-export const Gray = '#666666';
-export const Blue = '#0000EE';
+export const Black = '#2F3437';
+export const BlackGray = '#322C28';
+export const Gray = '#7B7C7D';
+export const Blue = '#0086CC';
 export const Pink = '#FF0000';
 
-export const TitleSize = '2.4rem';
-export const HeaderSize = '1.7rem';
-export const TopicSize = '1.3rem';
-export const SentenceSize = '1.1rem';
+export const TitleSize = '3rem';
+export const HeaderSize = '2.1rem';
+export const TopicSize = '1.5rem';
+export const SentenceSize = '1.2rem';
 
 export const CardSize = '22rem';
 export const MobileCardSize = '28rem';
@@ -18,3 +19,5 @@ export const hhuge = '1384px';
 export const large = '1450px';
 export const medium = '768px';
 export const small = '450px';
+
+export const IconSize = '1.2rem';
