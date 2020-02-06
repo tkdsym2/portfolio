512ffc1193af85a7072f9dd797e9dd96d0f265d5
tkdsym2 <k222.tkd.fms@gmail.com>
2020-02-06 22:40:18 +0900

    Feat: update url

--- components/MetaCard.jsx
+++ components/MetaCard.jsx
@@ -6,11 +6,12 @@ const MetaCard = ({ data }) => (
     <title>tkd.work | {data.title}</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta name="twitter:card" content={data.title}/>
-    <meta name="twitter:site" content="tkd.work" />
-    <meta property="og:url" content={"https://google.com/"}/>
+    <meta name="twitter:site" content={`https://tkd.work${data.url}`} />
+    <meta property="og:url" content={`https://tkd.work${data.url}`}/>
     <meta property="og:title" content={data.title}/>
-    <meta property="og:description" content="description"/>
+    <meta property="og:description" content={data.subtitle}/>
     <meta property="og:image" content={data.main} />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.icon"/>
   </Head>
 )
 
