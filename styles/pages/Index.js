b65c26323b648bf99b9fd9b7d67ca761266c0b32
Kazuma Takada <cs192010@meiji.ac.jp>
2022-10-30 02:08:39 +0900

    update all version

--- styles/pages/Index.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const DesktopFrame = styled.div`
-  width: 70%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  /* margin: 0.2rem 17rem 1rem 17rem; */
-`
